d988032d05f9566c8a167b16dd32129f88f179e0
Christopher Johnson <cjohn@ceh.ac.uk>
2014-06-27 15:42:56 +0100

    Adding a few basic tests and also defining the default fact of concat_basedir

--- spec/classes/selendroid_spec.rb
+++ spec/classes/selendroid_spec.rb
@@ -1,12 +1,45 @@
 require 'spec_helper'
 
 describe 'selendroid', :type => :class do 
-  let(:params) { {
-    :manage_user  => true,
-    :java_home    => '/java/home',
-    :android_home => '/android/home'
-  } }
+  let(:default_facts) {{
+    :concat_basedir => '/dne'
+  }}
   
-  it { should contain_user('selendroid') }
-  it { should contain_group('selendroid') }
+  describe 'when user is managed' do  
+    let(:params) { {
+      :manage_user  => true,
+      :java_home    => '/java/home',
+      :android_home => '/android/home'
+    } }
+      
+    it { should contain_user('selendroid') }
+  end
+
+  describe 'when group is managed' do
+    let(:params) { {
+      :manage_group => true,
+      :java_home    => '/java/home',
+      :android_home => '/android/home'
+    } }
+    it { should contain_group('selendroid') }
+  end
+
+  describe 'when user isnt managed' do
+    let(:params) { {
+      :manage_user  => false,
+      :java_home    => '/java/home',
+      :android_home => '/android/home'
+    } }
+      
+    it { should_not contain_user('selendroid') }
+  end
+
+  describe 'when group isnt managed' do
+    let(:params) { {
+      :manage_group => false,
+      :java_home    => '/java/home',
+      :android_home => '/android/home'
+    } }
+    it { should_not contain_group('selendroid') }
+  end
 end
